1f223c3acd78ac41cc51b273da66a15eabb1f17f
Mirko <moonsethra@gmail.com>
2024-11-25 18:39:33 -0500

    Change result

--- routes/auth.js
+++ routes/auth.js
@@ -64,6 +64,10 @@ router.get('/google/callback', async (req, res) => {
     } catch (error) {
         console.error('Error durante la autenticación de Google:', error.message);
         console.error('Stack Trace:', error.stack);
+        
+        // Limpiar los tokens si hay un error
+        req.session.tokens = null;
+        
         res.status(500).send('Authentication failed');
     }
 });
@@ -89,6 +93,10 @@ router.get('/refresh-token', async (req, res) => {
         res.json(newTokens.credentials); // Retornar los nuevos tokens
     } catch (error) {
         console.error('Error al refrescar el token de acceso:', error.message);
+        
+        // Limpiar los tokens si no se pueden refrescar
+        req.session.tokens = null;
+        
         res.status(500).send('Error al refrescar el token de acceso');
     }
 });
@@ -138,12 +146,17 @@ router.post('/send', async (req, res) => {
         res.status(200).send('Correo enviado con éxito');
     } catch (error) {
         console.error('Error al enviar correo:', error.message);
+        
+        // Limpiar los tokens si hay un error al enviar el correo
+        req.session.tokens = null;
+        
         res.status(500).send('Error al enviar correo');
     }
 });
 
 // Ruta para cerrar sesión y borrar los tokens guardados
 router.get('/logout', (req, res) => {
+    req.session.tokens = null;  // Eliminar los tokens de la sesión
     req.session.destroy((err) => {
         if (err) {
             return res.status(500).send('Error al cerrar sesión');
